8d6fb659f4041c2f54ac7d94f57b229008a1ab4d
Andrwe Lord Weber <lord-weber-andrwe@andrwe.org>
2014-11-18 15:11:41 +0100

    fix password image for OTP for non-default OC-URI
    Signed-off-by: Andrwe Lord Weber <lord-weber-andrwe@andrwe.org>

--- user_otp/js/utils.js
+++ user_otp/js/utils.js
@@ -8,7 +8,9 @@ $(document).ready(function(){
 		'<input id="otpPassword" type="number" placeholder="One Time Password" value=""' +
 			' name="otpPassword" original-title="" autocomplete="off" >' +
 		'<label class="infield" for="otpPassword" style="opacity: 1;">One Time Password</label>' +
-		'<img id="password-icon" class="svg" alt="" class="otppassword-icon" src="/core/img/actions/password.svg">' +
+		'<img id="password-icon" class="svg" alt="" class="otppassword-icon" src="' +
+			OC.filePath('core', 'img/actions', 'password.svg') +
+			'">' +
 		'</p>'
 	);
 	$("#submit").removeAttr("disabled");
